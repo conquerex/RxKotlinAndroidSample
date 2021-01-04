44b70a7b12011a9a5d73fa629b7fe1ed906bef20
Jongkook <conquerex@gmail.com>
2021-01-04 12:47:14 +0900

    Add UserAddActivity

--- app/src/main/java/what/the/rxkotlin/android/add/UserAddActivity.kt
+++ app/src/main/java/what/the/rxkotlin/android/add/UserAddActivity.kt
@@ -1,18 +1,20 @@
 package what.the.rxkotlin.android.add
 
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import android.view.inputmethod.EditorInfo
-import androidx.appcompat.app.AppCompatActivity
+import android.view.inputmethod.InputMethodManager
+import what.the.rxkotlin.android.BaseActivity
 import what.the.rxkotlin.android.databinding.ActivityUserAddBinding
 
-class UserAddActivity : AppCompatActivity() {
+class UserAddActivity : BaseActivity() {
     private lateinit var binding: ActivityUserAddBinding
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    override fun onCreateBaseActivity(savedInstanceState: Bundle?) {
         binding = ActivityUserAddBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
 
         binding.txtAddJob.setOnEditorActionListener { v, actionId, event ->
             var handled = false
@@ -20,7 +22,13 @@ class UserAddActivity : AppCompatActivity() {
                 handled = true
             }
             Log.i(this.localClassName, "* * * setOnEditorActionListener : $handled")
+
+            // 자동으로 키보드 내리기
+            val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(currentFocus?.windowToken, 0)
             return@setOnEditorActionListener handled
         }
+
     }
+
 }
