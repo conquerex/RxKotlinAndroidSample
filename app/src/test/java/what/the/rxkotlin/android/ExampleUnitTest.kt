08e0ece8c71b9d113a08b062bda86486456bf849
Jongkook <conquerex@gmail.com>
2021-02-04 20:57:44 +0900

    Unit test for REST API

--- app/src/test/java/what/the/rxkotlin/android/ExampleUnitTest.kt
+++ app/src/test/java/what/the/rxkotlin/android/ExampleUnitTest.kt
@@ -3,19 +3,11 @@ package what.the.rxkotlin.android
 import io.kotest.core.spec.Spec
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.core.test.TestCase
-import io.kotest.fp.success
-import io.kotest.matchers.nulls.shouldNotBeNull
-import kotlinx.coroutines.awaitAll
 import what.the.rxkotlin.android.apis.ApiClient
 import what.the.rxkotlin.android.apis.ApiService
-import java.util.concurrent.CountDownLatch
+import what.the.rxkotlin.android.data.UpdateItem
 import java.util.concurrent.TimeUnit
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 class ExampleUnitTest : FunSpec() {
     private lateinit var apiService: ApiService
 
@@ -34,7 +26,34 @@ class ExampleUnitTest : FunSpec() {
 
     init {
         test("getUsers API") {
+            // awaitDone() 함수가 편리한 이유 :
+            // test() 함수가 실행되는 스레드에서 onComplete() 함수를 호출할 때까지 기다려주기 때문
             val testObserver = apiService.getUsers()
+                .test()
+                .awaitDone(10000, TimeUnit.MILLISECONDS)
+
+            testObserver.assertNoErrors()
+                .assertComplete()
+        }
+
+        test("updateUser API") {
+            val item = UpdateItem()
+            item.name = "Sample"
+            item.job = "Developer"
+
+            val testObserver = apiService.updateUser(999, item)
+                .test()
+                .awaitDone(2000, TimeUnit.MILLISECONDS)
+            testObserver.assertNoErrors()
+                .assertComplete()
+        }
+
+        test("createUser API") {
+            val item = UpdateItem()
+            item.name = "Sample2"
+            item.job = "Android Developer"
+
+            val testObserver = apiService.createUser(item)
                 .test()
                 .awaitDone(2000, TimeUnit.MILLISECONDS)
             testObserver.assertNoErrors()
